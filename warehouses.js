00f1ade5ff6b9c2d5b080eaa8ff48aac7cab48a2
tomekszy <red_rose@poczta.fm>
2021-06-24 11:58:05 +0200

    uporządkowanie kodu, lista rodzajów towarów i jednostek

--- warehouses.js
+++ warehouses.js
@@ -3,8 +3,12 @@ const fs = require('fs');
 let rawwarehouses = fs.readFileSync('warehouses.json');
 let warehouses = JSON.parse(rawwarehouses);
 console.log("Liczba obiektów w pliku wejściowym", warehouses.length);
+const rodzajeTowaru = [];
+const jednostki = [];
 
 warehouses.forEach(element => {
+    rodzajeTowaru.push(element.rodzajTowaru);
+    jednostki.push(element.jednostka);
     if (element.rodzajTowaru == 'Puchary' || 'Wstążki do medali') {
         element.itemNumberDiv = element.itemNumber.split('/')
         element.itemNumber = element.itemNumberDiv[0];
@@ -20,6 +24,43 @@ warehouses.forEach(element => {
             element.fullName = element.fullName.replace(/\d{1,2}mm/, '')
         }
     }
+    element.idMagazynu = element._id
+    element.daneSpecyficzne = [{
+        nazwaRozmiaru: element.itemNumber2,
+        kolor: element.kolor,
+        size: element.size,
+        wysokosc: element.wysokosc,
+        szerokosc: element.szerokosc,
+        dlugosc: element.dlugosc,
+        cenaDetalicznaNetto: element.cenaDetalicznaNetto,
+        cenaDetalicznaBrutto: element.cenaDetalicznaBrutto,
+        cenaExportEuro: element.cenaExportEuro,
+        liczba: element.liczba,
+        wartoscWmagazynieNetto: element.wartoscWmagazynieNetto,
+        cenaZakupuNetto: element.cenaZakupuNetto,
+        cenaDetalicznaWaluta: element.cenaDetalicznaWaluta,
+        vat: element.vat,
+        jednostka: element.jednostka,
+        towarOpis: element.towarOpis,
+        jednostkaWymiaru: element.jednostkaWymiaru
+    }];
+    delete element._id
+    delete element.itemNumber2;
+    delete element.kolor;
+    delete element.size;
+    delete element.wysokosc;
+    delete element.szerokosc;
+    delete element.dlugosc;
+    delete element.cenaDetalicznaNetto;
+    delete element.cenaDetalicznaBrutto;
+    delete liczba;
+    delete wartoscWmagazynieNetto;
+    delete cenaZakupuNetto;
+    delete cenaDetalicznaWaluta;
+    delete vat;
+    delete jednostka;
+    delete towarOpis;
+    delete jednostkaWymiaru;
 })
 
 const lookup = warehouses.reduce((a, e) => {
@@ -44,86 +85,10 @@ arrToSave.forEach(element => {
     removeByAttr(warehouses, 'itemNumber', element.itemNumber)
 });
 
-arrToSave.forEach((element, index) => {
-    element.idMagazynu = element._id
-    element.daneSpecyficzne = [{
-        nazwaRozmiaru: element.itemNumber2,
-        kolor: element.kolor,
-        size: element.size,
-        wysokosc: element.wysokosc,
-        szerokosc: element.szerokosc,
-        dlugosc: element.dlugosc,
-        cenaDetalicznaNetto: element.cenaDetalicznaNetto,
-        cenaDetalicznaBrutto: element.cenaDetalicznaBrutto,
-        cenaExportEuro: element.cenaExportEuro,
-        liczba: element.liczba,
-        wartoscWmagazynieNetto: element.wartoscWmagazynieNetto,
-        cenaZakupuNetto: element.cenaZakupuNetto,
-        cenaDetalicznaWaluta: element.cenaDetalicznaWaluta,
-        vat: element.vat,
-        jednostka: element.jednostka,
-        towarOpis: element.towarOpis,
-        jednostkaWymiaru: element.jednostkaWymiaru
-    }];
-    delete element._id
-    delete element.itemNumber2;
-    delete element.kolor;
-    delete element.size;
-    delete element.wysokosc;
-    delete element.szerokosc;
-    delete element.dlugosc;
-    delete element.cenaDetalicznaNetto;
-    delete element.cenaDetalicznaBrutto;
-    delete liczba;
-    delete wartoscWmagazynieNetto;
-    delete cenaZakupuNetto;
-    delete cenaDetalicznaWaluta;
-    delete vat;
-    delete jednostka;
-    delete towarOpis;
-    delete jednostkaWymiaru;
-});
-
-warehouses.forEach((element, index) => {
-    element.idMagazynu = element._id
-    element.daneSpecyficzne = [{
-        nazwaRozmiaru: element.itemNumber2,
-        kolor: element.kolor,
-        size: element.size,
-        wysokosc: element.wysokosc,
-        szerokosc: element.szerokosc,
-        dlugosc: element.dlugosc,
-        cenaDetalicznaNetto: element.cenaDetalicznaNetto,
-        cenaDetalicznaBrutto: element.cenaDetalicznaBrutto,
-        cenaExportEuro: element.cenaExportEuro,
-        liczba: element.liczba,
-        wartoscWmagazynieNetto: element.wartoscWmagazynieNetto,
-        cenaZakupuNetto: element.cenaZakupuNetto,
-        cenaDetalicznaWaluta: element.cenaDetalicznaWaluta,
-        vat: element.vat,
-        jednostka: element.jednostka,
-        towarOpis: element.towarOpis,
-        jednostkaWymiaru: element.jednostkaWymiaru
-    }];
-    delete element._id
-    delete element.itemNumber2;
-    delete element.kolor;
-    delete element.size;
-    delete element.wysokosc;
-    delete element.szerokosc;
-    delete element.dlugosc;
-    delete element.cenaDetalicznaNetto;
-    delete element.cenaDetalicznaBrutto;
-    delete liczba;
-    delete wartoscWmagazynieNetto;
-    delete cenaZakupuNetto;
-    delete cenaDetalicznaWaluta;
-    delete vat;
-    delete jednostka;
-    delete towarOpis;
-    delete jednostkaWymiaru;
-});
-
+const rodzajeTowaruUnique = [...new Set(rodzajeTowaru)];
+const jednostkiUnique = [...new Set(jednostki)];
+console.log("Rodzaje towaru: ", rodzajeTowaruUnique);
+console.log("Jednostki: ", jednostkiUnique);
 console.log("Liczba obiektów niezduplikowanych", warehouses.length);
 console.log("Liczba obiektów zduplikowanych", arrToSave.length);
 const duplicates = JSON.stringify(arrToSave);
